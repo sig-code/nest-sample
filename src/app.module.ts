706092c58175a17785036bf3a451b640ce3a91bb
sig-code <m.takku29@gmail.com>
2022-06-12 14:59:52 +0900

    Dockerの設定の修正

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 import * as ormConfig from '../ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig)],
+  imports: [TypeOrmModule.forRoot(ormConfig), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
